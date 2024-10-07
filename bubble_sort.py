6b57fb8e85d381155c302f825033a3f1f072f316
KRRT7 <turcioskevinr@gmail.com>
2024-10-07 16:23:25 +0000

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,3 +6,27 @@ def sorter(arr):
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
     return arr
+
+
+def decompress_braces(string):
+    numbers = "123456789"
+    stack = []
+
+    for char in string:
+        if char in numbers:
+            stack.append(int(char))
+        elif char == "{":
+            continue
+        # Below condition tells about what to be done when we encounter alphabyte.
+        elif "a" <= char <= "z" or "A" <= char <= "Z":
+            stack.append(char)
+        # Here you can use "else" also, but for better understanding(to know when
+        # this code block will execute, i am keeping "elif" here).
+        elif char == "}":
+            segment = ""
+            while isinstance(stack[-1], str):
+                popped_char = stack.pop()
+                segment = popped_char + segment
+            num = stack.pop()
+            stack.append(segment * num)
+    return "".join(stack)
