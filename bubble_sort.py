4d51cae3bd51fc03bc81bc37af61f03208408f3a
Kevin Turcios <turcioskevinr@gmail.com>
2024-10-23 12:35:40 -0500

    recompress

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,3 +6,24 @@ def sorter(arr):
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
     return arr
+
+
+def decompress_braces(string):
+    numbers = "123456789"
+    stack = []
+
+    for char in string:
+        if char in numbers:
+            stack.append(int(char))
+        elif char == "{":
+            continue
+        elif "a" <= char <= "z" or "A" <= char <= "Z":
+            stack.append(char)
+        elif char == "}":
+            segment = ""
+            while isinstance(stack[-1], str):
+                popped_char = stack.pop()
+                segment = popped_char + segment
+            num = stack.pop()
+            stack.append(segment * num)
+    return "".join(stack)
