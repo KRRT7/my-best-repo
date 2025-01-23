3413b613487ae98e96f4a99f49fa85305ad545ea
Kevin Turcios <turcioskevinr@gmail.com>
2025-01-23 18:14:24 -0500

    Update to_cache.py

--- to_cache.py
+++ to_cache.py
@@ -9,14 +9,3 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
-
-
-@lru_cache(maxsize=32)
-def nth_prime(n):
-    prime_count = 0
-    num = 1
-    while prime_count < n:
-        num += 1
-        if is_prime(num):
-            prime_count += 1
-    return num
