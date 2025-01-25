cdce88ef3104be8d5fd63715931ef29bc301750c
Kevin Turcios <turcioskevinr@gmail.com>
2025-01-24 21:23:53 -0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,3 +7,18 @@ def sorter(arr):
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
     return arr
+
+
+def sorterv2(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
+    total_sum = sum(arr)
+
+    max_value = max(arr)
+
+    return arr, total_sum, max_value
