4dc5551dbc3abc147d0340a1a4fda9916eed911d
Kevin Turcios <turcioskevinr@gmail.com>
2025-02-05 19:14:49 -0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 def sorter(arr):
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
@@ -6,3 +9,15 @@ def sorter(arr):
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
     return arr
+
+
+@lru_cache(maxsize=None)
+def cached_function(arg):
+    """A simple function with lru_cache."""
+    return arg + 1, cached_function.cache_info()
+
+
+class TestClass:
+    @lru_cache(maxsize=None)
+    def cached_method(self, arg):
+        return arg + 2, self.cached_method.cache_info()
