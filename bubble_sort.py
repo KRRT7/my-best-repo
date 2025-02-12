18829d12a7d8cf2a4977ab23a7644fe9be09916c
Kevin Turcios <turcioskevinr@gmail.com>
2025-02-11 21:13:18 -0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,13 @@
 def sorter(arr):
     unused_var = None
+    print("codeflash")
     for i in range(len(arr)):
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
                 temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
+    print(arr)
     return arr
 
 
