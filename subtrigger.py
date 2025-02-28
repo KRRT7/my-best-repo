a0112527f6525c3db455d40dfcf0962e0c9202f4
Kevin Turcios <turcioskevinr@gmail.com>
2025-02-28 15:39:56 -0800

    Update subtrigger.py

--- subtrigger.py
+++ subtrigger.py
@@ -1,5 +1,16 @@
+import asyncio
+
+
 def run_echo():
     from subprocess import Popen, PIPE
 
     p = Popen(["echo", "Hello, world!"], stdout=PIPE)
     return p.communicate()[0]
+
+
+async def main():
+   return await asyncio.to_thread(run_echo)
+
+
+def run_async_echo():
+    return asyncio.run(main())
